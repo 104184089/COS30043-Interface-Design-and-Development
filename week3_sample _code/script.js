c35333cf1648d66d8c80406f8d185c17c0fed0a1
Vu Minh Quang <155576444+Quangden03@users.noreply.github.com>
2024-08-30 22:25:51 +0700

    COS30043 VueJS

--- week3_sample _code/script.js
+++
@@ -1,9 +0,0 @@
-Vue.createApp({
-    data() {
-        return {
-            message: "Hello Quang!",
-            myVar: "dogs",
-            task: ['task 1', 'task 2']
-        }
-    }
-}).mount("#app")
